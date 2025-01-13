9e591a2bf6b7f4a234199f552a96949d152ba633
arshia <arshiabohloli@gmail.com>
2025-01-13 15:50:21 +0330

    refctor config and installed air

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
 	cfg := config{
-		addr: "localhost:8080",
+		addr: os.Getenv("addr"),
 	}
 	app := &application{
 		config: cfg,
